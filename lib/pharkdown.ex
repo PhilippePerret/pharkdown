e03aee06ac65f0f654bf72f2b995eee2b2ecabf4
PhilippePerret <philippe.perret@yahoo.fr>
2025-01-06 05:17:22 +0100

    Premier dépôt

---
+++ lib/pharkdown.ex
@@ -0,0 +1,18 @@
+defmodule Pharkdown do
+  @moduledoc """
+  Documentation for `Pharkdown`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Pharkdown.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/pharkdown_test.exs
@@ -0,0 +1,8 @@
+defmodule PharkdownTest do
+  use ExUnit.Case
+  doctest Pharkdown
+
+  test "greets the world" do
+    assert Pharkdown.hello() == :world
+  end
+end
